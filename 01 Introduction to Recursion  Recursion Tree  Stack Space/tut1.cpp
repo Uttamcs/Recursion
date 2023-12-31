5284418399946de22763ef951a3134ca5a5b5956
Uttam Kumar <uk89197@gmail.com>
2023-12-31 20:27:45 +0530

    Lecture 01

---
+++ 01 Introduction to Recursion  Recursion Tree  Stack Space/tut1.cpp
@@ -0,0 +1,45 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void print()
+// {
+//     cout<<1<<endl;
+//     print();
+// }
+// int main()
+// {
+//     print();
+//     return 0;
+// }
+
+// // It will run infinite time . So we can call it is the case of Stack Overflow
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int counter = 0;
+void print()
+{
+    // Base condition
+    if(counter == 5)
+    {
+        return ;
+    }
+    counter++;
+    cout<<counter<<endl;
+    print();
+}
+int main()
+{
+    print();
+    return 0;
+}
+
+
+// It will run till counter becomes equal to 5
